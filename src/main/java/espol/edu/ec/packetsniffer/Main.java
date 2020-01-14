847a12c7974dcae4d2cf2b1493e299c1fda1731e
Kenny Camba Torres <kacamba@espol.edu.ec>
2020-01-14 02:04:15 -0500

    first commit

---
+++ src/main/java/espol/edu/ec/packetsniffer/Main.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package espol.edu.ec.packetsniffer;
+
+import com.sun.jna.Platform;
+import java.io.EOFException;
+import java.io.IOException;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.List;
+import java.util.concurrent.TimeoutException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.pcap4j.core.NotOpenException;
+import org.pcap4j.core.PacketListener;
+import org.pcap4j.core.PcapHandle;
+import org.pcap4j.core.PcapNativeException;
+import org.pcap4j.core.PcapNetworkInterface;
+import org.pcap4j.core.PcapNetworkInterface.PromiscuousMode;
+import org.pcap4j.core.PcapPacket;
+import org.pcap4j.core.PcapStat;
+import org.pcap4j.core.Pcaps;
+import org.pcap4j.packet.IpV4Packet;
+import org.pcap4j.packet.Packet;
+import org.pcap4j.util.NifSelector;
+
+/**
+ *
+ * @author kcamb
+ */
+public class Main {
+    static PcapNetworkInterface getNetworkDevice() {
+        PcapNetworkInterface device = null;
+        try {
+            device = new NifSelector().selectNetworkInterface();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return device;
+    }
+
+    public static void main(String[] args) throws PcapNativeException, NotOpenException {
+        PcapNetworkInterface device = getNetworkDevice();
+        System.out.println("You chose: " + device);
+        
+        if (device == null) {
+            System.out.println("No device chosen.");
+            System.exit(1);
+        }
+
+        // Open the device and get a handle
+        int snapshotLength = 65536; // in bytes   
+        int readTimeout = 50; // in milliseconds                   
+        final PcapHandle handle;
+        handle = device.openLive(snapshotLength, PromiscuousMode.PROMISCUOUS, readTimeout);
+
+        // Create a listener that defines what to do with the received packets
+        PacketListener listener = new PacketListener() {
+            @Override
+            public void gotPacket(PcapPacket packet) {
+                // Override the default gotPacket() function and process packet
+                System.out.println(packet.getTimestamp());
+                System.out.println(packet.get(IpV4Packet.class).getHeader());
+            }
+        };
+        
+        try {
+            int maxPackets = 50;
+            handle.loop(maxPackets, listener);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        PcapStat stats = handle.getStats();
+        System.out.println("Packets received: " + stats.getNumPacketsReceived());
+        System.out.println("Packets dropped: " + stats.getNumPacketsDropped());
+        System.out.println("Packets dropped by interface: " + stats.getNumPacketsDroppedByIf());
+        // Supported by WinPcap only
+        if (Platform.isWindows()) {
+            System.out.println("Packets captured: " +stats.getNumPacketsCaptured());
+        }
+        // Cleanup when complete
+        handle.close();
+    }
+}
